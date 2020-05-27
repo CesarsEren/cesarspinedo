238ddb3f1a01eef3c93b6d99b2c69de0dca8f6d0
CesarsEren <cpinedo428@gmail.com>
2020-05-27 14:11:51 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { AcercaComponent } from './views/acerca/acerca.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
+import { PortafolioComponent } from './views/portafolio/portafolio.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/Inicio', pathMatch: 'full' },
+  { path: "Inicio", component: InicioComponent }, 
+  { path: "Acerca", component: AcercaComponent },
+  { path: "Contacto",component: ContactoComponent},
+  { path: "Portafolio",component: PortafolioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './views/inicio/inicio.component';
+import { AcercaComponent } from './views/acerca/acerca.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
+import { PortafolioComponent } from './views/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AcercaComponent,
+    ContactoComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
